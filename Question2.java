c626b7ffdc804bc4ed35417d49317cf0e254958c
Mikhail Adam Zahadi <S10239784@connect.np.edu.sg>
2023-04-20 11:15:47 +0800

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -4,7 +4,6 @@ public class Question2
 {
   public static void main(String[] args)
   {
-  public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
     double height;
     height = in.nextDouble();
