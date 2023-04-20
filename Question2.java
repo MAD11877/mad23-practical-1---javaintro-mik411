e7b44c5f3c5642dc4c5c4cc78e40bf3811f6c28c
Mikhail Adam Zahadi <S10239784@connect.np.edu.sg>
2023-04-20 10:59:23 +0800

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -4,11 +4,9 @@ public class Question2
 {
   public static void main(String[] args)
   {
-    System.out.println("Enter Weight in KG");
     Scanner myWeight = new Scanner(System.in);
     double weight;
     weight = myWeight.nextDouble();
-    System.out.println("Enter Height in M");
     Scanner myHeight = new Scanner(System.in);
     double height;
     height = myHeight.nextDouble();
