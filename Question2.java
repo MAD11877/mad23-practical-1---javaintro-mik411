cd795c435b5a8958114e85d9701e6d200a8c530c
Mikhail Adam Zahadi <S10239784@connect.np.edu.sg>
2023-04-20 11:13:53 +0800

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -4,12 +4,13 @@ public class Question2
 {
   public static void main(String[] args)
   {
-    Scanner myHeight = new Scanner(System.in);
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
     double height;
-    height = myHeight.nextDouble();
-    Scanner myWeight = new Scanner(System.in);
+    height = in.nextDouble();
+    //Scanner myWeight = new Scanner(System.in);
     double weight;
-    weight = myWeight.nextDouble();
+    weight = in.nextDouble();
    // System.out.println("Enter Height in M");
     double bmi = weight / ( height * height);
     System.out.println(bmi);
